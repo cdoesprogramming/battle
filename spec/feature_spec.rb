76cb300d0fd85e81170d98c810767af0ce2830cc
Ed <ed321@hotmail.co.uk>
2022-02-09 15:50:42 +0000

    Implemented Rspec

---
+++ spec/feature_spec.rb
@@ -0,0 +1,6 @@
+describe "print Homepage", type: :feature do
+  it "returns Testing infrastructure working!" do
+    visit "/"
+    expect(page).to have_content "Testing infrastructure working!"
+  end
+end
