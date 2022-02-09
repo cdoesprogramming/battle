23b0ef849b4e298fbcc692192f6342fa7a510e9b
camilla <cgreene553@gmail.com>
2022-02-09 16:29:40 +0000

    Entering players

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,3 +4,13 @@ describe "print Homepage", type: :feature do
     expect(page).to have_content "Testing infrastructure working!"
   end
 end
+
+describe "names", type: :feature do
+  it "allows people to enter their name" do
+    visit "/"
+    fill_in "player_1", with: 'Camilla' 
+    fill_in "player_2", with: 'Ed'
+    click_button 'Submit'
+    expect(page).to have_content 'Camilla vs Ed'    
+  end
+end
